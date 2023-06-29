c7dd17f61e9556e29e300d8582d462d08ff80f56
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2023-06-29 10:11:28 +0530

    Next Commit

--- src/Home.js
+++ src/Home.js
@@ -24,11 +24,21 @@ function Home(){
                             <p>Embracing wellness, ensuring safety</p>
                         </div>
                         <div className="divNavigationButtonArea">
-                            <div className="Button"><img src="https://vectorified.com/images/home-icon-white-2.png" alt="" /></div>
-                            <div className="Button"><img src="https://nationalpid.com/wp-content/uploads/2018/07/about-us-icon-white.png" alt="" /></div>
-                            <div className="Button"><img src="https://iconsplace.com/wp-content/uploads/_icons/ffffff/256/png/package-icon-18-256.png" alt="" /></div>
-                            <div className="Button"><img src="https://www.seekpng.com/png/full/137-1373765_phone-icon-white-contact-us-icon-transparent-white.png" alt="" /></div>
-                            <div className="Button"><img src="https://www.drinkcraft.net/wp-content/uploads/2021/02/login-icon-white.png" alt="" /></div>
+                            <div className="Button"><img src="https://vectorified.com/images/home-icon-white-2.png" alt="" />
+                                    <figcaption>Home</figcaption>
+                            </div>
+                            <div className="Button"><img src="https://nationalpid.com/wp-content/uploads/2018/07/about-us-icon-white.png" alt="" />
+                                    <figcaption>Our Center</figcaption>
+                            </div>
+                            <div className="Button"><img src="https://iconsplace.com/wp-content/uploads/_icons/ffffff/256/png/package-icon-18-256.png" alt="" />
+                                    <figcaption>Packages</figcaption>
+                            </div>
+                            <div className="Button"><img src="https://www.seekpng.com/png/full/137-1373765_phone-icon-white-contact-us-icon-transparent-white.png" alt="" />
+                                    <figcaption>Contact Us</figcaption>
+                            </div>
+                            <div className="Button"><img src="https://www.drinkcraft.net/wp-content/uploads/2021/02/login-icon-white.png" alt="" />
+                                    <figcaption>Sign In</figcaption>
+                            </div>
                         </div>
                         <div className="divHomeSectionRightPara">
                             <p>Experience safety and wellness at our Quarantine Center. With a focus on your well-being, we provide a secure environment for your quarantine period. Rest easy knowing that our dedicated team is here to support your journey to optimal health.</p>
