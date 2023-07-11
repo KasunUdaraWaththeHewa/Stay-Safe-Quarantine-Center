911f4981c5d2f256c34b8d3b8b6160a6c2058615
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2023-07-11 23:31:19 +0530

    mongo db configuration done

--- backend/server.js
+++ backend/server.js
@@ -8,3 +8,18 @@ const app = express();
 const PORT=process.env.PORT//8070;
 app.use(cors());
 app.use(bodyParser.json());
+
+const URL=process.env.MONGODB_URL;
+mongoose.connect(URL,{
+    useCreateIndex:true,
+    useNewUrlParser:true,
+    useUnifiedTopologyL:true,
+    useFindAndModify:false
+})
+const connection=mongoose.connection;
+connection.once("open",()=>{
+    console.log("MongoDB Connection Success!");
+})
+app.listen(PORT,()=>{
+    console.log("Server is up and running on port ${PORT}")
+})
