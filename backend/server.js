a2eb80a38e63497b058da1486874832bc15b11e6
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2023-07-12 00:13:54 +0530

    trouble shooting

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+require("dotenv").config();
 const dotenv = require("dotenv");
-
 const app = express();
 const PORT=process.env.PORT//8070;
 app.use(cors());
@@ -13,13 +13,13 @@ const URL=process.env.MONGODB_URL;
 mongoose.connect(URL,{
     useCreateIndex:true,
     useNewUrlParser:true,
-    useUnifiedTopologyL:true,
+    useUnifiedTopology:true,
     useFindAndModify:false
-})
+});
 const connection=mongoose.connection;
 connection.once("open",()=>{
     console.log("MongoDB Connection Success!");
 })
 app.listen(PORT,()=>{
-    console.log("Server is up and running on port ${PORT}")
+    console.log(`Server is up and running on port: ${PORT}`);
 })
