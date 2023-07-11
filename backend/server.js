bd7a1f4c17653c83b0164ea82fd29c62ea97f045
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2023-07-11 22:21:31 +0530

    backend configuration done

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+const app = express();
+const PORT=process.env.PORT//8070;
+app.use(cors());
+app.use(bodyParser.json());
