5e7f71d0fb8dbacc53e537cfa630b47ff71e2505
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2023-07-12 20:16:20 +0530

    new commit

--- src/pages/Packages.js
+++ src/pages/Packages.js
@@ -12,83 +12,87 @@ import EnhancedPackage from '../img/EnhancedPackage.png'
 import FamilyPackage from '../img/FamilyPackage.png'
 import VIPPackage from '../img/VIPPackage.png'
 import LongTermPackage from '../img/LongTermPackage.png'
+
+
+
 const Packages = () => {
-    return ( 
+    return (
         <div className="packages">
             <div className="navbar_pkg">
-                <NavBar/>
+                <NavBar />
             </div>
             <div className="imagepackagecontainer">
                 <img src="https://img.freepik.com/premium-photo/happy-male-doctor-communicating-with-mature-patient-who-is-wheelchair-hallway-clinic_637285-690.jpg?w=1060" alt="" className="pkgimgbig" />
                 <h1 className="txtpkgAboveImage">Packages</h1>
             </div>
-            <Container fluid className = "pkgDetails">
-                <Row className = "pkgrow">
-                    <Col lg = {4} className = "pkgcol">
-                    <Card style={{ width: '25rem' }} className="basicpkg" border="light" >
-                        <Card.Img variant="top" src = {BasicPackage} />
-                        <Card.Body>
-                            <Card.Title className="basictitlecard" ><b>Basic Quarantine Package</b></Card.Title>
-                            <Card.Text>
-                                Stay comfortably in our well-appointed rooms, enjoy three delicious meals a day while our medical staff ensures your well-being with regular health check-ups.
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
+
+            <Container fluid className="pkgDetails">
+                <Row className="pkgrow">
+                    <Col lg={4} className="pkgcol">
+                        <Card style={{ width: '25rem' }} className="basicpkg" border="light" >
+                            <Card.Img variant="top" src={BasicPackage} />
+                            <Card.Body>
+                                <Card.Title className="basictitlecard" ><b>Basic Quarantine Package</b></Card.Title>
+                                <Card.Text>
+                                    Stay comfortably in our well-appointed rooms, enjoy three delicious meals a day while our medical staff ensures your well-being with regular health check-ups.
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
                     </Col>
-                    <Col lg = {4} className = "pkgcol">
-                    <Card style={{ width: '25rem' }} border="light" className="familypkg">
-                        <Card.Img variant="top" src = {FamilyPackage} />
-                        <Card.Body>
-                            <Card.Title className="familytitlecard"><b>Family Quarantine Package</b></Card.Title>
-                            <Card.Text>
-                                Our family-friendly accommodations provide dedicated family units, meals for all members, regular health check-ups, and a range of indoor and outdoor activities to enjoy.
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
+                    <Col lg={4} className="pkgcol">
+                        <Card style={{ width: '25rem' }} border="light" className="familypkg">
+                            <Card.Img variant="top" src={FamilyPackage} />
+                            <Card.Body>
+                                <Card.Title className="familytitlecard"><b>Family Quarantine Package</b></Card.Title>
+                                <Card.Text>
+                                    Our family-friendly accommodations provide dedicated family units, meals for all members, regular health check-ups, and a range of indoor and outdoor activities to enjoy.
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
                     </Col>
-                    <Col lg = {4} className = "pkgcol">
-                    <Card style={{ width: '25rem' }} border="light" className="longtermpkg">
-                        <Card.Img variant="top" src = {LongTermPackage} />
-                        <Card.Body>
-                            <Card.Title className="longtitlecard"><b>Long Quarantine Package</b></Card.Title>
-                            <Card.Text>
-                                Our long term package offers extended stay accommodations, nutritious meals tailored for longer stays and engaging activities to combat isolation.
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
+                    <Col lg={4} className="pkgcol">
+                        <Card style={{ width: '25rem' }} border="light" className="longtermpkg">
+                            <Card.Img variant="top" src={LongTermPackage} />
+                            <Card.Body>
+                                <Card.Title className="longtitlecard"><b>Long Quarantine Package</b></Card.Title>
+                                <Card.Text>
+                                    Our long term package offers extended stay accommodations, nutritious meals tailored for longer stays and engaging activities to combat isolation.
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
                     </Col>
 
                 </Row>
-                <Row className = "pkgrow">
-                    <Col lg = {6} className = "pkgcol">
-                    <Card style={{ width: '25rem' }} border="light" className="enhancedpkg">
-                        <Card.Img variant="top" src = {EnhancedPackage} />
-                        <Card.Body>
-                            <Card.Title className="enhancedtitlecard"><b>Enhanced Quarantine Package</b></Card.Title>
-                            <Card.Text>
-                                Elevate your quarantine experience with upgraded accommodations, a variety of dietary options, daily health monitoring, access to recreational activities, and complimentary Wi-Fi and entertainment.
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
+                <Row className="pkgrow">
+                    <Col lg={6} className="pkgcol">
+                        <Card style={{ width: '25rem' }} border="light" className="enhancedpkg">
+                            <Card.Img variant="top" src={EnhancedPackage} />
+                            <Card.Body>
+                                <Card.Title className="enhancedtitlecard"><b>Enhanced Quarantine Package</b></Card.Title>
+                                <Card.Text>
+                                    Elevate your quarantine experience with upgraded accommodations, a variety of dietary options, daily health monitoring, access to recreational activities, and complimentary Wi-Fi and entertainment.
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
                     </Col>
-                    <Col lg = {6} className = "pkgcol">
-                    <Card style={{ width: '25rem' }} border="light" className="vippkg">
-                        <Card.Img variant="top" src = {VIPPackage} />
-                        <Card.Body>
-                            <Card.Title className="viptitlecard"><b>VIP Quarantine Package</b></Card.Title>
-                            <Card.Text>
-                                Indulge in luxurious accommodations, personalized gourmet meals, round-the-clock medical staff, specialized wellness services, and concierge assistance to ensure a VIP experience throughout your stay.
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
+                    <Col lg={6} className="pkgcol">
+                        <Card style={{ width: '25rem' }} border="light" className="vippkg">
+                            <Card.Img variant="top" src={VIPPackage} />
+                            <Card.Body>
+                                <Card.Title className="viptitlecard"><b>VIP Quarantine Package</b></Card.Title>
+                                <Card.Text>
+                                    Indulge in luxurious accommodations, personalized gourmet meals, round-the-clock medical staff, specialized wellness services, and concierge assistance to ensure a VIP experience throughout your stay.
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
                     </Col>
                 </Row>
             </Container>
             <div className="footer_pkg">
-                <Footer/>
+                <Footer />
             </div>
         </div>
-     );
+    );
 }
- 
+
 export default Packages;
