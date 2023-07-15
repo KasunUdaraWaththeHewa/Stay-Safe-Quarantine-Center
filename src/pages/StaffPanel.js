2a9160e14e084ff08d81de5da6fd93a019adb21e
samudiperera09 <samudiperera125@gmail.com>
2023-07-16 00:46:03 +0530

    Added new components and styles

--- src/pages/StaffPanel.js
+++ src/pages/StaffPanel.js
@@ -15,27 +15,21 @@ function StaffPanel(){
                     <div className='divAdminPanelLeft'>
                         <div className='PanelRow'>
                             <div className="panelItem">
-                            <Link to="/nurse" className="link"><img src="https://cdn-icons-png.flaticon.com/512/204/204245.png" alt="" /></Link>
+                                <Link to="/nurse" className="link"><img src="https://cdn-icons-png.flaticon.com/512/204/204245.png" alt="" /></Link>
                                 <figcaption><b>Nurse</b></figcaption>
                             </div>
                             <div className="panelItem">
-                                <img src="https://cdn4.iconfinder.com/data/icons/ordinary-people/512/patient-512.png" alt="" />
+                            <Link to="/patient" className="link"><img src="https://cdn4.iconfinder.com/data/icons/ordinary-people/512/patient-512.png" alt="" /></Link>
                                 <figcaption><b>Patient</b></figcaption>
                             </div>
-                            
-                        </div>
-                        <div className='PanelRow'>
                             <div className="panelItem">
-                            <Link to="/doctor" className="link"><img src="https://cdn-icons-png.flaticon.com/512/194/194915.png" alt="" /></Link>
+                                <Link to="/doctor" className="link"><img src="https://cdn-icons-png.flaticon.com/512/194/194915.png" alt="" /></Link>
                                 <figcaption><b>Doctor</b></figcaption>
-
                             </div>
-                        </div>
-                        <div className='PanelRow'>
                             <div className="panelItem">
-                            <Link to="/staff" className="link"><img src="https://cdn3.iconfinder.com/data/icons/team-management/136/8-512.png" alt="" /></Link>
+                                <Link to="/staff" className="link"><img src="https://cdn3.iconfinder.com/data/icons/team-management/136/8-512.png" alt="" /></Link>
                                 <figcaption><b>Staff</b></figcaption>
-                            </div>                           
+                            </div>    
                         </div>
                     </div>
                     <div className='divAdminPanelRight'>
@@ -47,13 +41,19 @@ function StaffPanel(){
                                         <Accordion.Item eventKey="0">
                                             <Accordion.Header>What you do will be tracked</Accordion.Header>
                                             <Accordion.Body>
-                                            Please note that your actions within the system will be tracked for accountability and quality assurance purposes. This monitoring helps us maintain a secure and efficient environment for the benefit of our patients and the smooth functioning of the quarantine center. Thank you for your cooperation.
+                                            Please note that your actions within the system will be tracked for accountability and quality assurance purposes.Thank you for your cooperation.
                                             </Accordion.Body>
                                         </Accordion.Item>
                                         <Accordion.Item eventKey="1">
                                             <Accordion.Header>Ensure Security of the Data</Accordion.Header>
                                             <Accordion.Body>
-                                            Protecting the privacy of patient, doctor, staff, and nurse details is our top priority. Stringent security measures are in place to prevent any unauthorized access or leakage of sensitive data. Rest assured that all information is securely stored, encrypted, and accessible only to authorized personnel. Your confidentiality is of utmost importance to us.
+                                            Protecting the privacy of patient, doctor, staff, and nurse details is our top priority. Your confidentiality is of utmost importance to us.
+                                            </Accordion.Body>
+                                        </Accordion.Item>
+                                        <Accordion.Item eventKey="3">
+                                            <Accordion.Header>Keep user information up to date to ensure accuracy</Accordion.Header>
+                                            <Accordion.Body>
+                                            Regularly updating user details such as contact information, roles, and permissions helps ensure that the admin panel reflects the most current and accurate information about each user.
                                             </Accordion.Body>
                                         </Accordion.Item>
                                     </Accordion>
