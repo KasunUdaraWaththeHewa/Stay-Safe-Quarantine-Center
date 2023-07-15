3dcdf07aa6dd813cb3ae4c1432d740f4abe0e56c
samudiperera09 <samudiperera125@gmail.com>
2023-07-16 00:46:32 +0530

    Added new components and styles

--- src/pages/AdminPanel.js
+++ src/pages/AdminPanel.js
@@ -21,25 +21,22 @@ function AdminPanel(){
                                 <Link to="/patient" className="link"><img src="https://cdn4.iconfinder.com/data/icons/ordinary-people/512/patient-512.png" alt="" /></Link>
                                 <figcaption><b>Patient</b></figcaption>
                             </div>
-                            
-                        </div>
-                        <div className='PanelRow'>
                             <div className="panelItem">
-                            <Link to="/doctor" className="link"><img src="https://cdn-icons-png.flaticon.com/512/194/194915.png" alt="" /></Link>
+                                <Link to="/doctor" className="link"><img src="https://cdn-icons-png.flaticon.com/512/194/194915.png" alt="" /></Link>
                                 <figcaption><b>Doctor</b></figcaption>
 
                             </div>
                             <div className="panelItem">
-                            <Link to="/nurse" className="link"><img src="https://cdn-icons-png.flaticon.com/512/204/204245.png" alt="" /></Link>
+                                <Link to="/nurse" className="link"><img src="https://cdn-icons-png.flaticon.com/512/204/204245.png" alt="" /></Link>
                                 <figcaption><b>Nurse</b></figcaption>
                             </div>
-                        </div>
-                        <div className='PanelRow'>
                             <div className="panelItem">
-                            <Link to="/staff" className="link"><img src="https://cdn3.iconfinder.com/data/icons/team-management/136/8-512.png" alt="" /></Link>
+                                <Link to="/staff" className="link"><img src="https://cdn3.iconfinder.com/data/icons/team-management/136/8-512.png" alt="" /></Link>
                                 <figcaption><b>Staff</b></figcaption>
-                            </div>                           
+                            </div> 
+                            
                         </div>
+
                     </div>
                     <div className='divAdminPanelRight'>
                         <div className="PanelRightPart">                               
@@ -49,13 +46,25 @@ function AdminPanel(){
                                         <Accordion.Item eventKey="0">
                                             <Accordion.Header>Create Staff Accounts by yourself</Accordion.Header>
                                             <Accordion.Body>
-                                            For enhanced security, it is advisable for the admin to handle the creation of staff accounts personally. This allows for the implementation of stringent security measures, accurate assignment of roles and permissions, and verification of user identities. By maintaining control over staff account creation, the admin can effectively mitigate the risk of unauthorized access and protect the confidentiality of sensitive information.
+                                            For enhanced security, it is advisable for the admin to handle the creation of staff accounts personally. 
                                             </Accordion.Body>
                                         </Accordion.Item>
                                         <Accordion.Item eventKey="1">
+                                            <Accordion.Header>Ensure each user has a unique password for the account</Accordion.Header>
+                                            <Accordion.Body>
+                                            Unique passwords help prevent unauthorized access and protect sensitive information associated with user accounts.
+                                            </Accordion.Body>
+                                        </Accordion.Item>
+                                        <Accordion.Item eventKey="2">
                                             <Accordion.Header>Ensure Security of the Data</Accordion.Header>
                                             <Accordion.Body>
-                                            Protecting the privacy of patient, doctor, staff, and nurse details is our top priority. Stringent security measures are in place to prevent any unauthorized access or leakage of sensitive data. Rest assured that all information is securely stored, encrypted, and accessible only to authorized personnel. Your confidentiality is of utmost importance to us.
+                                            Protecting the privacy of patient, doctor, staff, and nurse details is our top priority. Your confidentiality is of utmost importance to us.
+                                            </Accordion.Body>
+                                        </Accordion.Item>
+                                        <Accordion.Item eventKey="3">
+                                            <Accordion.Header>Keep user information up to date to ensure accuracy</Accordion.Header>
+                                            <Accordion.Body>
+                                            Regularly updating user details such as contact information, roles, and permissions helps ensure that the admin panel reflects the most current and accurate information about each user.
                                             </Accordion.Body>
                                         </Accordion.Item>
                                     </Accordion>
