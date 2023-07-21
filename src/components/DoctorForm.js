7721012b97048203937f888d226d7abf62c6dc0e
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2023-07-21 21:26:55 +0530

    option button files added to create

--- src/components/DoctorForm.js
+++ src/components/DoctorForm.js
@@ -7,90 +7,90 @@ import axios from 'axios';
 
 function DoctorForm() {
   const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [doctorID, setDoctorID] = useState("");
-    const [phoneNumber, setPhoneNumber] = useState("");
-    const [email, setEmail] = useState("");
-    const [medicalLicenseNumber, setMedicalLicenseNumber] = useState("");
-    const [specialization, setSpecilization] = useState("");
-    const [experience, setExperience] = useState("");
-    const [address, setAddress] = useState("");
-    const [avalibleDays, setAvalibleDays] = useState("");
-    const [emergencycontactNumber, setEmergencycontactNumber] = useState("");
-    const [gender, setGender] = useState("");
-    const [Relationship, setRelationship] = useState("");
-    const [skills, setSkills] = useState("");
-    
-    function sendData(e){
-      e.preventDefault();
-      const newDoctor ={
-        firstName,lastName,doctorID,phoneNumber,email,medicalLicenseNumber,specialization,experience,address,avalibleDays,emergencycontactNumber,gender,Relationship,skills
-      }
-      axios.post("http://localhost:8070/doctor/add",newDoctor).then(()=>{
-        alert("Doctor added");
-        setFirstName("")
-        setLastName("")
-        setDoctorID("")
-        setPhoneNumber("")
-        setEmail("")
-        setMedicalLicenseNumber("")
-        setSpecilization("")
-        setExperience("")
-        setAddress("")
-        setAvalibleDays("")
-        setEmergencycontactNumber("")
-        setGender("")
-        setRelationship("")
-        setSkills("")
-      }).catch((err)=>{
-        alert(err)
-      })
+  const [lastName, setLastName] = useState("");
+  const [doctorID, setDoctorID] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [email, setEmail] = useState("");
+  const [medicalLicenseNumber, setMedicalLicenseNumber] = useState("");
+  const [specialization, setSpecilization] = useState("");
+  const [experience, setExperience] = useState("");
+  const [address, setAddress] = useState("");
+  const [avalibleDays, setAvalibleDays] = useState("");
+  const [emergencycontactNumber, setEmergencycontactNumber] = useState("");
+  const [gender, setGender] = useState('Male');
+  const [Relationship, setRelationship] = useState("Married");
+  const [skills, setSkills] = useState(null);
+
+  function sendData(e) {
+    e.preventDefault();
+    const newDoctor = {
+      firstName, lastName, doctorID, phoneNumber, email, medicalLicenseNumber, specialization, experience, address, avalibleDays, emergencycontactNumber, gender, Relationship, skills
     }
+    axios.post("http://localhost:8070/doctor/add", newDoctor).then(() => {
+      alert("Doctor added");
+      setFirstName("")
+      setLastName("")
+      setDoctorID("")
+      setPhoneNumber("")
+      setEmail("")
+      setMedicalLicenseNumber("")
+      setSpecilization("")
+      setExperience("")
+      setAddress("")
+      setAvalibleDays("")
+      setEmergencycontactNumber("")
+      setGender("")
+      setRelationship("")
+      setSkills("")
+    }).catch((err) => {
+      alert(err)
+    })
+  }
 
   return (
     <Form>
       <Row className="mb-3">
         <Col>
-            <Form.Label>First name</Form.Label>
-          <Form.Control placeholder="First name" onChange={(e) => setFirstName(e.target.value)}  />
+          <Form.Label>First name</Form.Label>
+          <Form.Control placeholder="First name" onChange={(e) => setFirstName(e.target.value)} />
         </Col>
         <Col>
-            <Form.Label>Last name</Form.Label>
-          <Form.Control placeholder="Last name"  onChange={(e) => setLastName(e.target.value)}  />
+          <Form.Label>Last name</Form.Label>
+          <Form.Control placeholder="Last name" onChange={(e) => setLastName(e.target.value)} />
         </Col>
       </Row>
 
       <Row className="mb-3">
         <Col>
-            <Form.Label>Doctor ID</Form.Label>
-          <Form.Control placeholder="Doctor ID" onChange={(e) => setDoctorID(e.target.value)}  />
+          <Form.Label>Doctor ID</Form.Label>
+          <Form.Control placeholder="Doctor ID" onChange={(e) => setDoctorID(e.target.value)} />
         </Col>
         <Col>
-            <Form.Label>Phone number</Form.Label>
-          <Form.Control placeholder="Phone number"  onChange={(e) => setPhoneNumber(e.target.value)} />
+          <Form.Label>Phone number</Form.Label>
+          <Form.Control placeholder="Phone number" onChange={(e) => setPhoneNumber(e.target.value)} />
         </Col>
       </Row>
 
       <Row className="mb-3">
         <Form.Group as={Col} controlId="formGridEmail">
           <Form.Label>Email</Form.Label>
-          <Form.Control type="email" placeholder="Email"  onChange={(e) => setEmail(e.target.value)} />
+          <Form.Control type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
         </Form.Group>
 
         <Col>
-            <Form.Label>Medical license Number</Form.Label>
-          <Form.Control placeholder="Medical license Number" onChange={(e) => setMedicalLicenseNumber(e.target.value)}  />
+          <Form.Label>Medical license Number</Form.Label>
+          <Form.Control placeholder="Medical license Number" onChange={(e) => setMedicalLicenseNumber(e.target.value)} />
         </Col>
       </Row>
 
       <Row className="mb-3">
         <Col>
-            <Form.Label>Specialization</Form.Label>
-          <Form.Control placeholder="Specialization" onChange={(e) => setSpecilization(e.target.value)}  />
+          <Form.Label>Specialization</Form.Label>
+          <Form.Control placeholder="Specialization" onChange={(e) => setSpecilization(e.target.value)} />
         </Col>
         <Col>
-            <Form.Label>Professional Experience</Form.Label>
-          <Form.Control placeholder="Professional Experience" onChange={(e) => setExperience(e.target.value)}  />
+          <Form.Label>Professional Experience</Form.Label>
+          <Form.Control placeholder="Professional Experience" onChange={(e) => setExperience(e.target.value)} />
         </Col>
       </Row>
 
@@ -101,50 +101,51 @@ function DoctorForm() {
 
       <Row className="mb-3">
         <Col>
-            <Form.Label>Available Days</Form.Label>
-          <Form.Control placeholder="Available Days" onChange={(e) => setAvalibleDays(e.target.value)}  />
+          <Form.Label>Available Days</Form.Label>
+          <Form.Control placeholder="Available Days" onChange={(e) => setAvalibleDays(e.target.value)} />
         </Col>
         <Col>
-            <Form.Label>Emergency Contact number</Form.Label>
-          <Form.Control placeholder="Emergency Contact number" onChange={(e) => setEmergencycontactNumber(e.target.value)}  />
+          <Form.Label>Emergency Contact number</Form.Label>
+          <Form.Control placeholder="Emergency Contact number" onChange={(e) => setEmergencycontactNumber(e.target.value)} />
         </Col>
       </Row>
       <Row className="mb-3">
         <Col >
-            <Form.Group as={Col} controlId="formGridState">
-                <Form.Label>Gender</Form.Label>
-                <Form.Select defaultValue="Male">
-                    <option>Male</option>
-                    <option>Female</option>
-                </Form.Select>
-            </Form.Group>
+          <Form.Group as={Col} controlId="formGridState">
+            <Form.Label>Gender</Form.Label>
+            <Form.Select value={gender} onChange={(e) => setGender(e.target.value)}>
+              <option value="Male">Male</option>
+              <option value="Female">Female</option>
+            </Form.Select>
+          </Form.Group>
         </Col>
         <Col >
-            <Form.Group as={Col} controlId="formGridState">
-                <Form.Label>Relationship</Form.Label>
-                <Form.Select defaultValue="Male">
-                    <option>Married</option>
-                    <option>Unmarried</option>
-                </Form.Select>
-            </Form.Group>
+          <Form.Group as={Col} controlId="formGridState">
+            <Form.Label>Relationship</Form.Label>
+
+            <Form.Select value={relationship} onChange={(e) => setRelationship(e.target.value)}>
+              <option value="Married">Married</option>
+              <option value="Unmarried">Unmarried</option>
+            </Form.Select>
+          </Form.Group>
         </Col>
-        
+
       </Row>
 
       <Form.Group controlId="formFile" className="mb-3">
-        <Form.Label>Enter skills , Training and Certification details in pdf format</Form.Label>
-        <Form.Control type="file"  onChange={(e) => setSkills(e.target.value)} />
+        <Form.Label>Enter skills, Training, and Certification details in pdf format</Form.Label>
+        <Form.Control type="file" onChange={(e) => setSkills(e.target.files[0])} />
       </Form.Group>
       <br />
       <Row>
-            <Col>
-                <Form.Group className="mb-3" id="formGridCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group>
-            </Col>
-            <Col>
-            
-            </Col>
+        <Col>
+          <Form.Group className="mb-3" id="formGridCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+        </Col>
+        <Col>
+
+        </Col>
       </Row>
       <br />
       <br />
@@ -153,7 +154,7 @@ function DoctorForm() {
       <Button variant="primary">Update</Button>{' '}
       <Button variant="danger">Delete</Button>{' '}
       <Button variant="success">Clear</Button>{' '}
-      
+
     </Form>
   );
 }
