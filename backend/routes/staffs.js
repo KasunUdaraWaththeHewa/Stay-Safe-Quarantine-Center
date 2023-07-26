ba7561487a787c25f2501f8a722760da572b1e14
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2023-07-26 22:29:34 +0530

    new commit

--- backend/routes/staffs.js
+++ backend/routes/staffs.js
@@ -40,11 +40,11 @@ router.route("/add").post((req,res)=>{
 router.route("/get/:staffID").get(async (req, res) => {
   let staffID = req.params.staffID;
   const staffMember = await staff.findOne({ staffID })
-    .then((staff) => {
-      if (!staff) {
+    .then((staffMember) => {
+      if (!staffMember) {
         return res.status(404).json({ error: "Staff member not found" });
       }
-      res.status(200).json({ status: "Staff member fetched", staff });
+      res.status(200).json({ status: "Staff member fetched", staffMember });
     })
     .catch((err) => {
       console.log(err.message);
