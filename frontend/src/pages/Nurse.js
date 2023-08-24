a8301e40931a9b45eaf3b4884579b3f4ca1e23e9
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2023-08-24 21:30:33 +0530

    nurse existing cards added

--- frontend/src/pages/Nurse.js
+++ frontend/src/pages/Nurse.js
@@ -1,29 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import '../css file/Nurse.css';
-import NavBar from "../components/NavBar";
-import NurseForm from "../components/NurseForm";
+import NavBar from '../components/NavBar';
+import NurseForm from '../components/NurseForm';
 import Footer from '../components/Footer';
+import axios from 'axios';
 
-function Nurse(){
-    return(
-        <div>
-            <div className="navBarContainor" ><NavBar /></div>
-            <div className="NurseDetailArea">
-                <div className="h2Holder"><h2><b>Nurse Details Collecting Form</b></h2></div>
-                <div className="formOneContainorNurse">
-                    <div className="nurseForm">
-                        <NurseForm/>
-                    </div>
-                    <div className="existingNurse">
+function Nurse() {
+  const [nurses, setNurses] = useState([]);
 
-                    </div>
-                </div>
-            </div>
-            <div className="footerContainorNurse">
-                    <Footer/>
-            </div>
+  useEffect(() => {
+    async function fetchNurses() {
+      try {
+        const response = await axios.get('http://localhost:8070/nurse/');
+        setNurses(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetchNurses();
+  }, []);
+
+  return (
+    <div>
+      <div className="navBarContainor">
+        <NavBar />
+      </div>
+      <div className="NurseDetailArea">
+        <div className="h2Holder">
+          <h2>
+            <b>Nurse Details Collecting Form</b>
+          </h2>
         </div>
-    );
-    
+        <div className="formOneContainorNurse">
+          <div className="nurseForm">
+            <NurseForm />
+          </div>
+          <div className="existingNurse">
+            <ul>
+              {nurses.map((nurse) => (
+                <div key={nurse.nurseID} className="existingNurseCard">
+                <p>Nurse Name: {nurse.firstName} {nurse.lastName}</p>
+                <p>Nurse ID: {nurse.nurseID}</p>
+                </div>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="footerContainorNurse">
+        <Footer />
+      </div>
+    </div>
+  );
 }
+
 export default Nurse;
