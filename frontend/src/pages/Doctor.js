2fc78d61bd217412ded55866a321e0efb9c0dffb
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2023-08-24 20:39:43 +0530

    doctor front end modified

--- frontend/src/pages/Doctor.js
+++ frontend/src/pages/Doctor.js
@@ -10,10 +10,17 @@ function Doctor(){
             <div className="navBarContainor" ><NavBar /></div>
             <div className="DoctorDetailArea">
                 <div className="h2Holder"><h2><b>Doctor Details Collecting Form</b></h2></div>
-                <div className="formOneContainorDoctor"><DoctorForm/></div>
-                <div className="footerContainor">
-                    <Footer/>
+                <div className="formOneContainorDoctor">
+                    <div className="doctorForm">
+                        <DoctorForm/>
+                    </div>
+                    <div className="existingDoctors">
+
+                    </div>
                 </div>
+            </div>
+            <div className="footerContainor">
+                    <Footer/>
             </div>
         </div>
     );
