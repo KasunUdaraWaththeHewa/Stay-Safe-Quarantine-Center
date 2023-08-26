52dee3c46ebc1f826fc840b01bd2300c94ce4b6d
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2023-08-26 15:22:50 +0530

    login and sugnup done for backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,15 +1,28 @@
+const { JsonWebTokenError } = require('jsonwebtoken');
 const User = require('../models/UserModel');
+const jwt= require('jsonwebtoken');
+
+const createToken=(_id)=>{
+    return jwt.sign({_id},process.env.SECRET,{expiresIn:'3d'})
+}
 
 const loginUser = async (req, res) => {
-    res.json({mssg: 'Login user'});
+    const {email, password}=req.body;
+    try{
+        const user = await User.login(email, password);
+        const token=createToken(user._id)
+        res.status(200).json(email, token);
+    }catch(err){
+        res.status(400).json({mssg: err.message});
+    }
 }
 
 const signupUser = async (req, res) => {
     const {email, password} = req.body;
-
     try{
         const user = await User.signup(email, password);
-        res.status(200).json(email, user);
+        const token=createToken(user._id)
+        res.status(200).json(email, token);
     }catch(err){
         res.status(400).json({mssg: err.message});
     }
--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -16,7 +16,6 @@ const userSchema = new Schema({
 });    
 
 userSchema.statics.signup = async function(email, password){
-
     if(!email ||!password){
         throw Error("All fields must be filled.");
     }
@@ -31,10 +30,25 @@ userSchema.statics.signup = async function(email, password){
     if(exists){
         throw new Error("Email already exists");
     }
-
     const salt = await bcrypt.genSalt(10);
     const hash = await bcrypt.hash(password, salt);
     const user = await this.create({email, password:hash});
+    return user;
+};
+
+
+userSchema.statics.login = async function(email, password){
+    if(!email ||!password){
+        throw Error("All fields must be filled.");
+    }
+    const user=await this.findOne({email});
+    if(!user){
+        throw new Error("Incorrect Email");
+    }
+    const match=await bcrypt.compare(password, user.password);
+    if(!match){
+        throw Error("Incorrect Password");
+    }
     return user;
 };
 
