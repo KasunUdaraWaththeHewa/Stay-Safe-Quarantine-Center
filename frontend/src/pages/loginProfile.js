4592bddb036fdcf47a76f1879b94db9763025ae2
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2023-08-27 17:55:47 +0530

    currently doing on useLogin and useLogout

--- frontend/src/pages/loginProfile.js
+++ frontend/src/pages/loginProfile.js
@@ -5,14 +5,17 @@ import img_p from '../img/admin_p.png';
 import Footer from '../components/Footer';
 import {Link} from 'react-router-dom';
 import { useState } from 'react';
+import { useLogin } from '../hooks/useLogin';
+
 
 export default function Login() {
   const [email,setEmail]=useState('')
   const [password,setPassword]=useState('')
+  const {login,error, isLoading}=useLogin()
 
   const handleSubmit = async(e)=>{
     e.preventDefault()
-    console.log(email,password)
+    await login(email,password)
   }
 
   return (
@@ -27,7 +30,8 @@ export default function Login() {
                   <input type="email" name="email" required onChange={(e)=>setEmail(e.target.value)} value={email}></input> <br></br>
                   <label>Password</label><br></br>
                   <input type="password" name="password" required onChange={(e)=>setPassword(e.target.value)} value={password}></input><br></br>
-                  <div className='submitBtn'><b><Link to="/adminPanel" className="link">Log in</Link></b></div>
+                  <div className='submitBtn'><b><Link to="/adminPanel" className="link" disabled = {isLoading}>Log in</Link></b></div>
+                  {error && <div className='error'>{error}</div>}
               </form>
           </div>
       </div>
