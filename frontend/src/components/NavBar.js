d1b99301867d768875263a049c6a324f022a36a2
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2023-08-28 21:23:03 +0530

    fixing bugs

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -23,7 +23,9 @@ function NavBar(){
                     <div className='navBarItemList'>
                         <div className="navBarItem"><b><Link to="/OurCenter" className="link">Our Center</Link></b></div>
                         <div className="navBarItem"><b><Link to="/packages" className="link">Packages</Link></b></div>
-                        <div className="navBarItem"><b><Link to="/contactus" className="link">Contact Us</Link></b></div>
+                        <div className="navBarItem"><b><Link to="/contactus" className="link">Contact Us</Link></b></div>    
+                        <div className="navBarItem"><b><Link to="/login" className="link">Login</Link></b></div>
+                        <div className="navBarItem"><b><Link to="/signup" className="link">Sign-Up</Link></b></div>
                         <div className="navBarItem"><b><Link to="/Sign_in_profile" className="link">Sign-In</Link></b></div>
                     </div>
                 {user &&(
