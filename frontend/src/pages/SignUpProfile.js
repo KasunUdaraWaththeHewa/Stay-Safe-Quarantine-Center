f0145dea9bf5acfd0b1ff6d2d9ec77e1aa7e91d7
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2023-08-28 17:33:00 +0530

    new

--- frontend/src/pages/SignUpProfile.js
+++ frontend/src/pages/SignUpProfile.js
@@ -11,7 +11,7 @@ import { useSignup } from '../hooks/useSignup';
 export default function Signup() {
   const [email,setEmail]=useState('')
   const [password,setPassword]=useState('')
-  const [signup,error,isLoading]=useSignup()
+  const {signup,error,isLoading}=useSignup()
 
   const handleSubmit = async(e)=>{
     e.preventDefault()
