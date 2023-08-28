14e87f78e82b765792a8dd775e3a1d9ab05c4ed7
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2023-08-28 23:23:33 +0530

    bugs fixed for some extenct

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,6 @@ const PORT=process.env.PORT || 8070;
 app.use(cors());
 app.use(bodyParser.json());
 
-
 const URL=process.env.MONGODB_URL;
 mongoose.connect(URL,{
     //useCreateIndex:true,
@@ -25,7 +24,7 @@ connection.once("open",()=>{
 
 const patientRouter=require("./routes/patients.js");
 app.use("/patient",patientRouter);
-const doctorRouter=require("./routes/doctorRoute.js");
+const doctorRouter=require("./routes/doctors.js");
 app.use("/doctor",doctorRouter);
 const nurseRouter=require("./routes/nurses.js");
 app.use("/nurse",nurseRouter);
