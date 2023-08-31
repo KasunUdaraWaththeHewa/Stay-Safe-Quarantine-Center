2a425ced1f80c878fab75f3b0f4ed9785d7e6fa4
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2023-08-31 17:25:16 +0530

    fixd style error in two forms

--- frontend/src/pages/SignUpProfile.js
+++ frontend/src/pages/SignUpProfile.js
@@ -3,7 +3,7 @@ import '../css file/signup.css';
 import NavBar from '../components/NavBar';
 import img_p from '../img/admin_p.png';
 import Footer from '../components/Footer';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import { useSignup } from '../hooks/useSignup';
 import Button from 'react-bootstrap/Button';
@@ -12,55 +12,56 @@ import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 
 export default function Signup() {
-  const [email,setEmail]=useState('')
-  const [password,setPassword]=useState('')
-  const [role,setRole]=useState('')
-  const {signup,error,isLoading}=useSignup()
-      
-  const handleSubmit = async(e)=>{
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [role, setRole] = useState('')
+  const { signup, error, isLoading } = useSignup()
+
+  const handleSubmit = async (e) => {
     e.preventDefault()
-    await signup({email,password,role});
+    await signup({ email, password, role });
   }
 
   return (
     <div>
-      <NavBar/>
+      <NavBar />
       <div className="signupBack">
-          <div className="signupS">
-              <img src={img_p} alt="signupProfile"></img>
-              <form className='signup' onSubmit={handleSubmit}>
-                  <h3>Sign Up</h3>
-                  <label>Email</label>
-                  <input
-                  type="email"
-                  required 
-                  onChange={(e)=>setEmail(e.target.value)}
-                  value={email}
-                  />
-                  <label>Password</label>
-                  <input
-                  type="password"
-                  required 
-                  onChange={(e)=>setPassword(e.target.value)}
-                  value={password}
-                  />
-                  <div className="roleDiv">
-                      <Form.Group as={Col} controlId="formGridState">
-                            <Form.Label>Role</Form.Label>
-                            <Form.Select value={role}id='roleInput' onChange={(e) => setRole(e.target.value)}>
-                              <option value="staff">staff</option>
-                              <option value="admin">admin</option>
-                              <option value="kitchen">kitchen Staff</option>
-                              <option value="pharmacy">pharmacy</option>
-                            </Form.Select>
-                      </Form.Group>
-                  </div>
-                  <button type="submit" onClick={handleSubmit} className='submitBtn' disabled={isLoading}><b>Sign Up</b></button>
-                  {error && <div className='error'>{error}</div>}
-              </form>
-          </div>
+        <div className="signupS">
+          <img src={img_p} alt="signupProfile"></img>
+          <form className='signup' onSubmit={handleSubmit}>
+            <h3>Sign Up</h3>
+            <label>Email</label>
+            <input
+              type="email"
+              required
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            />
+            <label>Password</label>
+            <input
+              type="password"
+              required
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+            />
+            <div className="roleDiv">
+              <Form.Group as={Col} controlId="formGridState">
+                <Form.Label>Role</Form.Label>
+                <Form.Select value={role} id='roleInput' onChange={(e) => setRole(e.target.value)}>
+                  <option value="staff">staff</option>
+                  <option value="admin">admin</option>
+                  <option value="kitchen">kitchen Staff</option>
+                  <option value="pharmacy">pharmacy</option>
+                </Form.Select>
+              </Form.Group>
+            </div>
+
+            <button type="submit" onClick={handleSubmit} className='submitBtn' disabled={isLoading}><b>Sign Up</b></button>
+            {error && <div className='error'>{error}</div>}
+          </form>
+        </div>
       </div>
-      <Footer/>
+      <Footer />
     </div>
   )
 }
--- frontend/src/pages/loginProfile.js
+++ frontend/src/pages/loginProfile.js
@@ -3,91 +3,67 @@ import '../css file/login.css';
 import NavBar from '../components/NavBar';
 import img_p from '../img/admin_p.png';
 import Footer from '../components/Footer';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import { useLogin } from '../hooks/useLogin';
-
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
 
 export default function Login() {
-  const [email,setEmail]=useState('')
-  const [password,setPassword]=useState('')
-  const [role,setRole]=useState('')
-  const {login,error, isLoading}=useLogin()
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [role, setRole] = useState('')
+  const { login, error, isLoading } = useLogin()
 
-  const handleSubmit = async(e)=>{
+  const handleSubmit = async (e) => {
     e.preventDefault()
-    await login({email,password,role})
+    await login({ email, password, role })
   }
 
   return (
     <div>
-      <NavBar/>
+      <NavBar />
       <div className="loginBack">
-          <div className="loginS">
-              <img src={img_p} alt="signinProfile"></img>
-              <form className='signin' onSubmit={handleSubmit}>
-                  <h3>Sign In</h3>
-                  <label>Email</label>
-                  <input
-                  type="email"
-                  required 
-                  onChange={(e)=>setEmail(e.target.value)}
-                  value={email}
-                  />
-                  <label>Password</label>
-                  <input
-                  type="password"
-                  required 
-                  onChange={(e)=>setPassword(e.target.value)}
-                  value={password}
-                  />
+        <div className="loginS">
+          <img src={img_p} alt="signinProfile"></img>
+          <form className='signin' onSubmit={handleSubmit}>
+            <h3>Sign In</h3>
+            <label>Email</label>
+            <input
+              type="email"
+              required
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            />
+            <label>Password</label>
+            <input
+              type="password"
+              required
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+            />
 
-                  <label>Role</label>
-                  <div className='roleDiv'>
-                      <input
-                        type="radio"
-                        required
-                        onChange={(e) => setRole(e.target.value)}
-                        value="staff"
-                        checked={role === "staff"}
-                      />
-                      <span>Staff</span>
+            <div className="roleDiv">
+              <Form.Group as={Col} controlId="formGridState">
+                <Form.Label>Role</Form.Label>
+                <Form.Select value={role} id='roleInput' onChange={(e) => setRole(e.target.value)}>
+                  <option value="staff">staff</option>
+                  <option value="admin">admin</option>
+                  <option value="kitchen">kitchen Staff</option>
+                  <option value="pharmacy">pharmacy</option>
+                </Form.Select>
+              </Form.Group>
+            </div>
 
-                      <input
-                        type="radio"
-                        required
-                        onChange={(e) => setRole(e.target.value)}
-                        value="admin"
-                        checked={role === "admin"}
-                      />
-                      <span>Admin</span>
 
-                      <input
-                        type="radio"
-                        required
-                        onChange={(e) => setRole(e.target.value)}
-                        value="kitchen"
-                        checked={role === "kitchen"}
-                      />
-                      <span>Kitchen</span>
-
-                      <input
-                        type="radio"
-                        required
-                        onChange={(e) => setRole(e.target.value)}
-                        value="Pharmacy"
-                        checked={role === "Pharmacy"}
-                      />
-                      <span>Pharmacist</span>
-                  </div>
-
-            
-                  <button type="submit" className='submitBtn' disabled={isLoading} onClick={handleSubmit}><b>Log in</b></button>
-                  {error && <div className='error'>{error}</div>}
-              </form>
-          </div>
+            <button type="submit" className='submitBtn' disabled={isLoading} onClick={handleSubmit}><b>Log in</b></button>
+            {error && <div className='error'>{error}</div>}
+          </form>
+        </div>
       </div>
-      <Footer/>
+      <Footer />
     </div>
   )
 }
