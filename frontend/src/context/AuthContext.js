5698b42a62663a75c138f4ff1aec513ddb1504c6
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2023-09-04 10:26:18 +0530

    solved the problem in code

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -15,16 +15,8 @@ export const authReducer = (state, action) => {
 
 export const AuthContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(authReducer, { 
-    user: null
+    user: JSON.parse(localStorage.getItem('user') ?? '{}')
   })
-
-  useEffect(() => {
-    const user = JSON.parse(localStorage.getItem('user'))
-
-    if (user) {
-      dispatch({ type: 'LOGIN', payload: user }) 
-    }
-  }, [])
 
   console.log('AuthContext state:', state)
   
