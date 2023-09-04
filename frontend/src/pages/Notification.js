20f2afcf3b022127e7b3599da9ef8ad30df6d17d
ThushankaPramuditha <thushankapramuditha17@gmail.com>
2023-09-04 12:18:15 +0530

    new commits to Notification.js

---
+++ frontend/src/pages/Notification.js
@@ -0,0 +1,77 @@
+import '../css file/Notification.css';
+import NavBar from '../components/NavBar';
+import NurseForm from '../components/NotificationForm';
+import Footer from '../components/Footer';
+import axios from 'axios';
+import React, { useContext, useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
+import NotificationForm from '../components/NotificationForm';
+
+function Notification() {
+  const { user } = useContext(AuthContext);
+  console.log(user);
+  const config = {
+    headers: {
+      Authorization: `Bearer ${user.token}`
+    }
+  };
+  const [nurses, setNotification] = useState([]);
+
+  useEffect(() => {
+    async function fetchNotification() {
+      try {
+        const response = await axios.get('http://localhost:8070/notification',config);
+        setNotification(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetchNotification();
+  }, []);
+
+  console.log(user);
+  const navigate = useNavigate();
+    if (!user || ((user.role !== 'admin')&&(user.role !== 'staff'))) {
+        navigate('/login');
+        return null;
+      }
+  
+  return (
+    <div>
+      <div className="navBarContainor">
+        <NavBar />
+      </div>
+      <div className="NotificationDetailArea">
+        <div className="h2Holder">
+          <h2>
+            <b>Notification Form</b>
+          </h2>
+        </div>
+        <div className="formOneContainorNotification">
+          <div className="notificationForm">
+            <NotificationForm />
+          </div>
+          <div className="existingNotification">
+            <div className='scrollablePanel'>
+              <ul>
+                {notifications.map((notification) => (
+                  <div key={notification.NotificationID} className="existingNotificationCard">
+                  <p>{notification.receiver}</p>
+                  <p>{notification.notificationID}</p>
+                  </div>
+                ))}
+              </ul>
+             </div> 
+          </div>
+        </div>
+      </div>
+      <div className="footerContainorNotification">
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default Notification;
