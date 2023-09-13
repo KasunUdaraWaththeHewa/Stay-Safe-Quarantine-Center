1c0aea238581a2c2aadd490292466345e80add8e
shyamalHAD <shyamalHAD@gmail.com>
2023-09-13 19:05:33 +0530

    new commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+
 import ContactUs from './pages/ContactUs';
 
 import Home_page from './pages/Home_page';
@@ -8,7 +9,7 @@ import AdminPanel from './pages/AdminPanel';
 
 import StaffPanel from './pages/StaffPanel';
 
-import PharmacyPanel from './pages/PharmacyPanel';
+import PharmacyPanal from './pages/PharmacyPanal';
 
 import Packages from './pages/Packages';
 
@@ -20,108 +21,44 @@ import Doctor from './pages/Doctor';
 import Patient from './pages/Patient';
 import Equipment from './pages/Equipment';
 import Payment from './pages/Payment';
-
-
-import Sign_in_pharmacy from './pages/Sign_in_pharmacy';
-
-import Sign_in_profile from './pages/Sign_in_profile'
-import Sign_in_doctor from './pages/Sign_in_doctor'
-import Sign_in_nurse from './pages/Sign_in_nurse'
-import Sign_in_patient from './pages/Sign_in_patient'
-import Sign_in_staff from './pages/Sign_in_staff'
 import OurCenter from './pages/OurCenter';
-import Sign_in_admin from './pages/Sign_in_admin';
 import Meals from './pages/Meals';
-
+import Kitchen from './pages/Kitchen_staff';
 import Login from './pages/loginProfile';
 import Signup from './pages/SignUpProfile';
-import NavBar from './components/NavBar';
+import ChangePassword from './pages/changePassword';
+import AddPackages from './pages/PackageAdd'
+import Notification from './pages/Notification';
+import PackageFetch from './pages/packageFetch';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Switch>
-          <Route exact path="/">
-            <Home_page />
-          </Route>
-          <Route exact path="/contactus">
-            <ContactUs />
-          </Route>
-          <Route exact path="/adminpanel">
-            <AdminPanel />
-          </Route>
-          <Route exact path="/staffpanel">
-            <StaffPanel />
-          </Route>
-          <Route exact path="/doctor">
-            <Doctor />
-          </Route>
-          <Route exact path="/staff">
-            <Staff />
-          </Route>
-          <Route exact path="/nurse">
-            <Nurse />
-          </Route>
-          <Route exact path="/patient">
-            <Patient />
-          </Route>
-          <Route exact path="/equipment">
-            <Equipment />
-          </Route>
-          <Route exact path="/packages">
-            <Packages />
-          </Route>
-          <Route exact path="/payment">
-            <Payment />
-          </Route>
-          <Route exact path="/Sign_in_profile">
-            <Sign_in_profile />
-          </Route>
-          <Route exact path="/Sign_in_doctor">
-            <Sign_in_doctor />
-          </Route>
+        <Routes>
+          <Route path="/" element={<Home_page />} />
+          <Route path="/contactus" element={<ContactUs />} />
+          <Route path="/adminpanel" element={<AdminPanel />} />
+          <Route path="/staffpanel" element={<StaffPanel />} />
+          <Route path="/doctor" element={<Doctor />} />
+          <Route path="/staff" element={<Staff />} />
+          <Route path="/nurse" element={<Nurse />} />
+          <Route path="/patient" element={<Patient />} />
+          <Route path="/equipment" element={<Equipment />} />
+          <Route path="/packages" element={<Packages />} />
+          <Route path="/payment" element={<Payment />} />
+          <Route path="/OurCenter" element={<OurCenter />} />
+          <Route path="/PharmacyPanal" element={<PharmacyPanal />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/meals" element={<Meals />} />
+          <Route path="/kitchen" element={<Kitchen />} />
+          <Route path="/changePassword" element={<ChangePassword />} />
+          <Route path="/addPackages" element={<AddPackages />} />
+          <Route path="/notification" element={<Notification />} />
+          <Route path="/packageFetch" element={<PackageFetch />} />
+        </Routes>
 
-          <Route exact path="/Sign_in_staff">
-            <Sign_in_staff />
-          </Route>
-
-          <Route exact path="/Sign_in_nurse">
-            <Sign_in_nurse />
-          </Route>
-
-          <Route exact path="/Sign_in_patient">
-            <Sign_in_patient />
-          </Route>
-
-          <Route exact path="/OurCenter">
-            <OurCenter />
-          </Route>
-          <Route exact path="/sign_in_admin">
-            <Sign_in_admin />
-          </Route>
-
-          <Route exact path="/Sign_in_pharmacy">
-            <Sign_in_pharmacy />
-          </Route>
-          <Route exact path="/PharmacyPanel">
-            <PharmacyPanel />
-          </Route>
-          <Route exact path="/signup">
-            <Signup />
-          </Route>
-
-          <Route exact path="/login">
-            <Login />
-          </Route>
-
-          <Route exact path="/meals">
-            <Meals />
-          </Route>
-
-
-
-        </Switch>
       </Router>
 
     </div>
