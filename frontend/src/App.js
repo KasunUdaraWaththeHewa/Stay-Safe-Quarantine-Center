13eb87e9d11986f6d6fc2f64c141139d1f1e696d
shyamalHAD <shyamalHAD@gmail.com>
2023-09-13 17:17:39 +0530

    commeting New

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ContactUs from './pages/ContactUs';
 
 import Home_page from './pages/Home_page';
@@ -21,6 +21,7 @@ import Patient from './pages/Patient';
 import Equipment from './pages/Equipment';
 import Payment from './pages/Payment';
 
+
 import Sign_in_pharmacy from './pages/Sign_in_pharmacy';
 
 import Sign_in_profile from './pages/Sign_in_profile'
@@ -34,92 +35,95 @@ import Meals from './pages/Meals';
 
 import Login from './pages/loginProfile';
 import Signup from './pages/SignUpProfile';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
       <Router>
-            <Switch>
-              <Route exact path="/">
-                  <Home_page/>
-              </Route>
-              <Route exact path="/contactus">
-                  <ContactUs/>
-              </Route>
-              <Route exact path="/adminpanel">
-                  <AdminPanel/>
-              </Route>
-              <Route exact path="/staffpanel">
-                  <StaffPanel/>
-              </Route>
-              <Route exact path="/doctor">
-                  <Doctor/>
-              </Route>
-              <Route exact path="/staff">
-                  <Staff/>
-              </Route>
-              <Route exact path="/nurse">
-                  <Nurse/>
-              </Route>
-              <Route exact path="/patient">
-                  <Patient/>
-              </Route>
-              <Route exact path="/equipment">
-                  <Equipment/>
-              </Route>
-              <Route exact path = "/packages">
-                  <Packages/>
-              </Route>
-              <Route exact path="/payment">
-                  <Payment/>
-              </Route>
-              <Route exact path="/Sign_in_profile">
-                  <Sign_in_profile/>
-              </Route>
-              <Route exact path="/Sign_in_doctor">
-                <Sign_in_doctor/>
-              </Route>
+        <Switch>
+          <Route exact path="/">
+            <Home_page />
+          </Route>
+          <Route exact path="/contactus">
+            <ContactUs />
+          </Route>
+          <Route exact path="/adminpanel">
+            <AdminPanel />
+          </Route>
+          <Route exact path="/staffpanel">
+            <StaffPanel />
+          </Route>
+          <Route exact path="/doctor">
+            <Doctor />
+          </Route>
+          <Route exact path="/staff">
+            <Staff />
+          </Route>
+          <Route exact path="/nurse">
+            <Nurse />
+          </Route>
+          <Route exact path="/patient">
+            <Patient />
+          </Route>
+          <Route exact path="/equipment">
+            <Equipment />
+          </Route>
+          <Route exact path="/packages">
+            <Packages />
+          </Route>
+          <Route exact path="/payment">
+            <Payment />
+          </Route>
+          <Route exact path="/Sign_in_profile">
+            <Sign_in_profile />
+          </Route>
+          <Route exact path="/Sign_in_doctor">
+            <Sign_in_doctor />
+          </Route>
 
-              <Route exact path="/Sign_in_staff">
-                <Sign_in_staff/>
-              </Route>
+          <Route exact path="/Sign_in_staff">
+            <Sign_in_staff />
+          </Route>
 
-              <Route exact path="/Sign_in_nurse">
-                <Sign_in_nurse/>
-              </Route>
+          <Route exact path="/Sign_in_nurse">
+            <Sign_in_nurse />
+          </Route>
 
-              <Route exact path="/Sign_in_patient">
-                <Sign_in_patient/>
-              </Route>
+          <Route exact path="/Sign_in_patient">
+            <Sign_in_patient />
+          </Route>
 
-              <Route exact path="/OurCenter">
-                <OurCenter/>
-              </Route>
-              <Route exact path="/sign_in_admin">
-                <Sign_in_admin/>
-              </Route>
+          <Route exact path="/OurCenter">
+            <OurCenter />
+          </Route>
+          <Route exact path="/sign_in_admin">
+            <Sign_in_admin />
+          </Route>
 
-              <Route exact path="/Sign_in_pharmacy">
-                <Sign_in_pharmacy/>
-              </Route>
-              <Route exact path="/PharmacyPanal">
-                <PharmacyPanal/>
-              </Route>
-              <Route exact path="/signup">
-                <Signup/>
-              </Route>
+          <Route exact path="/Sign_in_pharmacy">
+            <Sign_in_pharmacy />
+          </Route>
+          <Route exact path="/PharmacyPanal">
+            <PharmacyPanal />
+          </Route>
+          <Route exact path="/signup">
+            <Signup />
+          </Route>
 
-              <Route exact path="/login">
-                <Login/>
-              </Route>
-              
-              <Route exact path="/meals">
-                <Meals/>
-              </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
 
-            </Switch>
+          <Route exact path="/meals">
+            <Meals />
+          </Route>
+
+
+
+        </Switch>
       </Router>
-      
+
     </div>
   );
 }
--- frontend/src/pages/PharmacyPanal.js
+++ frontend/src/pages/PharmacyPanal.js
@@ -1,47 +1,193 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NavBar from '../components/NavBar';
 import '../css file/PharmacyPanal.css'
+
 
 import Accordion from 'react-bootstrap/Accordion';
 import { Link } from 'react-router-dom';
 
 
 function PharmacyPanal() {
+    const [popupAdd, stepop1] = useState(false);
+    const [popupViwe, stepop2] = useState(false);
+    const [popupUpdate, stepop3] = useState(false);
+    const [popupDel, stepop4] = useState(false);
+    const add = () => {
+        stepop1(!popupAdd);
+    };
+    const viwe = () => {
+        stepop2(!popupViwe);
+    };
+    const update = () => {
+        stepop3(!popupUpdate);
+    }
+    const del = () => {
+        stepop4(!popupDel);
+    }
     return (
         <div>
-            <div><NavBar /></div>
+            <NavBar> </NavBar>
             <div className="panalBody">
-                <button>Add medicine</button>
-                <button>Delete medicin</button>
-                <button>Update medicin</button>
-                <button>Viwe medicin</button>
-                <hr />
-                <div>
-                    <form className="popup">
-                        <h1>Adding</h1>
-                        <label htmlFor="name">ID:</label>
-                        <input type="text" name="name" /><br />
-                        <label htmlFor="name">Name:</label>
-                        <input type="text" name="name" /><br />
-                        <label htmlFor="name">Quntity:</label>
-                        <input type="text" name="name" /><br />
+                <div className="funcBtn">
+                    <button className='Btn' onClick={add}>Add</button>
+                    <button className='Btn' onClick={viwe}>Viwe</button>
+                    <button className='Btn' onClick={update}>Update</button>
+                    <button className='Btn' onClick={del}>Delete</button>
 
-                        
 
-                        <label htmlFor="country">Type:</label>
-                        <select name="country" id="country">                            
-                            <option value="nepal">Tablet</option>
-                            <option value="usa">Liuid</option>
-                            <option value="australia">item</option>
-                        </select><br /><br />
-
-                        <input type="submit" value="Submit" />
-                    </form>
                 </div>
+                {popupAdd ?
+                    <div className="popup">
+                        <br />
+
+                        <button onClick={add} className='closePopup'> Close</button>
+                        <br />
+                        <br />
+                        <div className="content">
+                            <form className='formEdit'>
+                                <label>
+                                    Name:
+                                    <input type="text" name="name" />
+                                </label>
+                                <br />
+                                <label>
+                                    ID:
+                                    <input type="text" name="id" />
+                                </label>
+                                <br />
+                                <label>
+                                    Date:
+                                    <input type="date" name="date" />
+                                </label>
+                                <br />
+                                <label>
+                                    Quntity:
+                                    <input type="text" name="quntity" />
+
+                                </label>
+                                <br />
+                                <br />
+                                <div className="lastBtn">
+                                    <input type="submit" value="Submit" className="submit" />
+                                    <input type="reset" className="submit" />
+                                </div>
+                            </form>
+                        </div>
+                    </div> : ""}
+                {popupViwe ?
+                    <div className="popup">
+                        <br />
+
+                        <button onClick={viwe} className='closePopup'> Close</button>
+                        <br />
+                        <br />
+                        <div className="content">
+                            <form className='formEdit'>
+                                <label>
+                                    Search:
+                                    <input type="search" name="name" />
+                                </label>
+                                <br />
+                                <div >
+                                    <table className='tbl'>
+                                        <tr>
+                                            <th>Name</th>
+                                            <th>Id</th>
+                                            <th>Quntity</th>
+                                        </tr>
+                                        <tr>
+                                            <td>Name</td>
+                                            <td>Id</td>
+                                            <td>Quntity</td>
+                                        </tr>
+                                    </table>
+                                </div>
+                                <div className="lastBtn">
+                                    <input type="submit" value="Submit" className="submit" />
+                                    <input type="reset" className="submit" />
+                                </div>
+                            </form>
+                        </div>
+                    </div> : ""}
+                {popupUpdate ?
+                    <div className="popup">
+                        <br />
+
+                        <button onClick={update} className='closePopup'> Close</button>
+                        <br />
+                        <br />
+                        <div className="content">
+                            <form className='formEdit'>
+                                <label>
+                                    Name:
+                                    <input type="text" name="name" />
+                                </label>
+                                <br />
+                                <label>
+                                    ID:
+                                    <input type="text" name="id" />
+                                </label>
+                                <br />
+                                <label>
+                                    Date:
+                                    <input type="date" name="date" />
+                                </label>
+                                <br />
+                                <label>
+                                    Quntity:
+                                    <input type="text" name="quntity" />
+
+                                </label>
+                                <br />
+                                <br />
+                                <div className="lastBtn">
+                                    <input type="submit" value="Submit" className="submit" />
+                                    <input type="reset" className="submit" />
+                                </div>
+                            </form>
+                        </div>
+                    </div> : ""}
+                {popupDel ?
+                    <div className="popup">
+                        <br />
+
+                        <button onClick={del} className='closePopup'> Close</button>
+                        <br />
+                        <br />
+                        <div className="content">
+                            <form className='formEdit'>
+                                <label>
+                                    Name:
+                                    <input type="text" name="name" />
+                                </label>
+                                <br />
+                                <label>
+                                    ID:
+                                    <input type="text" name="id" />
+                                </label>
+                                <br />
+                                <label>
+                                    Date:
+                                    <input type="date" name="date" />
+                                </label>
+                                <br />
+                                <label>
+                                    Quntity:
+                                    <input type="text" name="quntity" />
+
+                                </label>
+                                <br />
+                                <br />
+                                <div className="lastBtn">
+                                    <input type="submit" value="Submit" className="submit" />
+                                    <input type="reset" className="submit" />
+                                </div>
+                            </form>
+                        </div>
+                    </div> : ""}
 
 
             </div>
-
 
         </div>
     );
